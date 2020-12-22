d5763311446e7482a0765df443dc9dc788aea4ee
KirillLutsenko <mnrovski68@gmail.com>
2020-12-22 14:38:26 +0200

    add vars, add transition for header menu(fixes main.js)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,3 @@ const burgerNav = document.querySelector('.burger');
 burger.addEventListener('click', function() {
   burgerNav.classList.toggle('burger-active');
 });
-
-console.dir(document.body.children[0].children[0]);
-
